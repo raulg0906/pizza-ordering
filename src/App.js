aca3bd25e1f7269afc253354e7b91fdd95ec18bc
raulg0906 <raulgonzalez1770@gmail.com>
2019-07-22 15:03:54 -0700

    added google fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './App.css';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+body{
+  font-family: 'Open Sans', sans-serif;
+}
+
+h1, h2, h3{
+  font-family: 'Righteous', cursive;
+}
+`
+
 
 function App() {
   return (
+   <>
+   <h1>Sliceline</h1>
+   <GlobalStyle/>
     <div>Hello Sliceline</div>
+  </>
   );
+
 }
 
 export default App;
