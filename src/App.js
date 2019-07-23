f5de17a9e56208e1426b4c8ffa6115db73bbb939
raulg0906 <raulgonzalez1770@gmail.com>
2019-07-22 17:53:58 -0700

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components'
+import { Navbar } from './Navbar/Navbar'
 
 const GlobalStyle = createGlobalStyle`
 body{
   font-family: 'Open Sans', sans-serif;
+  margin: 0;
 }
 
 h1, h2, h3{
@@ -15,8 +17,8 @@ h1, h2, h3{
 function App() {
   return (
    <>
-   <h1>Sliceline</h1>
    <GlobalStyle/>
+   <Navbar/>
     <div>Hello Sliceline</div>
   </>
   );
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components'
+import {pizzaRed} from "../Styles/colors"
+import { Title } from "../Styles/title"
+
+ const NavbarStyled =  styled.div `
+    background: ${pizzaRed};
+    padding: 10px;
+`
+const Logo = styled(Title) `
+    font-size: 20px;
+    color: white;
+    text-shadow: 3px -1px 4px black;
+`
+export function Navbar(){
+    return <NavbarStyled>
+        <Logo>
+            SliceLine 🍕
+        </Logo>
+    </NavbarStyled>
+}
