c9b829f7ed943d81fb6b55533c99d2a8aeb3d079
raulg0906 <raulgonzalez1770@gmail.com>
2019-07-22 19:08:27 -0700

    added menu items

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -6,5 +6,30 @@ export const foods = [
     {
         name: 'Pepperoni Pizza',
         img: '/img/pizza2.jpeg'
+    },
+    {
+        name:'CheeseBurger',
+        img: '/img/burger.jpeg'
+    },
+    {
+        name: 'Chicken Fingers',
+        img: '/img/chicken-fingers.jpeg'
+    },
+    {
+        name:'Chicken Pizza',
+        img:'/img/chicken-pizza.jpeg'
+    },
+    {
+        name:'Fries',
+        img:'/img/fries.jpeg'
+    },
+    {
+        name:'Gyro',
+        img:'img/gyro.jpeg'
+    },
+    {
+        name:'Sandwich',
+        img: 'img/sandwich.jpeg'
     }
+
 ]
