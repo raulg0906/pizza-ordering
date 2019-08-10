b82dbcca56e5de8fa00d0d575a5d722c47113c3e
raulg0906 <raulgonzalez1770@gmail.com>
2019-08-10 10:23:06 -0700

    added some of dialog box

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Navbar } from './Navbar/Navbar';
 import {Banner } from './Banner/Banner';
 import {Menu} from './Menu/Menu';
 import {GlobalStyle} from './Styles/GlobalStyle';
-
+import { FoodDialog } from './FoodDialog/FoodDialog'
 
 function App() {
+const [openFood, setOpenFood] = useState();
+
   return (
    <>
    <GlobalStyle/>
+   <FoodDialog openFood={openFood}/>
    <Navbar/>
    <Banner/>
-   <Menu/>
-    <div></div>
+   <Menu setOpenFood={setOpenFood} />
   </>
   );
 
---
+++ src/FoodDialog/FoodDialog.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components' ;
+
+const Dialog = styled.div `
+width: 500px;
+height: 2000px;
+background-color: white;
+position: fixed;
+top: 75px;
+z-index: 5;
+max-height: calc(100% - 100px);
+left: calc(50% - 250px);
+`
+
+const DialogShadow=styled.div`
+position: fixed;
+height: 100%;
+width 100%;
+top: 0px;
+backgorund-color: black;
+opacity: 0.7;
+z-index: 4;
+
+`
+
+const DialogBanner = styled.div `
+min-height: 200px;
+margin-bottom: 20px;
+${({img}) => `background-image: url(${img})` };
+background-position: center;
+background-size: cover;
+`
+
+
+export function FoodDialog({openFood}){
+    return  ( 
+        openFood ? ( 
+         <>
+            <DialogShadow/>
+            <Dialog><DialogBanner img={openFood.img}/>
+            </Dialog>
+        </>
+            ) : null
+        );
+}
